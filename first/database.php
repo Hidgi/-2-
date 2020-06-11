61a28be4f70600af59c552254aa15445a1e41aec
Hidgi <59579597+Hidgi@users.noreply.github.com>
2020-06-11 16:39:33 +0300

    Update database.php

--- first/database.php
+++ first/database.php
@@ -1,6 +1,18 @@
 <?php
-$link = mysqli_connect('localhost', 'hidgi', '1234', 'playground');
-    if (!$link) {
-          die("Connection failed: " . mysqli_connect_error());
-    }
+echo "123";
+$host="localhost"
+$user="hidgi"
+$pas="1234"
+$db="playground"
+$link = new mysqli($host, $user, $pas);
+if (!$link->connect_error) {
+	die("Connection failed: " . $link->connect_error());
+}
+$link->query("use ".$db);
+if ($link->query("use ".$db) === TRUE) {
+  echo "success";
+} else {
+  echo "Error: " . $link->error;
+}
 ?>
+
