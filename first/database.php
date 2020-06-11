c6070c4bf1a3ba4f88ad36ef00df6a15a84de2b4
Hidgi <59579597+Hidgi@users.noreply.github.com>
2020-06-11 16:15:48 +0300

    Update database.php

--- first/database.php
+++ first/database.php
@@ -1,5 +1,5 @@
 <?php
-$link = mysqli_connect('localhost', 'hidgi', '1234', 'database');
+$link = mysqli_connect('localhost', 'hidgi', '1234', 'playground');
     if (!$link) {
           die("Connection failed: " . mysqli_connect_error());
     }
